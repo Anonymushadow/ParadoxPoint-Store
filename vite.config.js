ce870a4485e3bedffd28a19954670d689249a31b
anonymushadow <claudelnahuel22@gmail.com>
2026-04-01 23:29:41 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   resolve: {
     alias: {
       '@components': '/src/components',  /* Cuando vite vean @components, lo va a reemplazar por /src/components */
